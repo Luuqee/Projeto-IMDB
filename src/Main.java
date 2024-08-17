e16a1ca5bcd0f0a2df873a5086ea8275237588b4
Igor ujiie <igorkazuhico@hotmail.com>
2024-08-16 22:51:08 -0300

    subindo estrutura do menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int opcaoEscolhida = 0;
+        do {
+            System.out.println("Digite a opção escolhida: ");
+            System.out.println("Opção 1 - Cadastrar novo autor");
+            System.out.println("Opção 2 - Buscar autor por nome");
+            System.out.println("Opção 3 - Cadastrar novo livro");
+            System.out.println("Opção 0 - Sair");
+            opcaoEscolhida = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcaoEscolhida) {
+                case 1:
+                    System.out.println();
+                    System.out.println("Iniciando cadastro de autor");
+                    System.out.println("Digite o nome do autor: ");
+                    String nomeAutor = scanner.nextLine();
+                    System.out.println();
+
+                    break;
+                case 2:
+                    System.out.println("Iniciando busca de autor");
+                    System.out.println("Digite o nome do autor que deseja buscar: ");
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Iniciando cadastro de livro");
+                    System.out.println("Digite o nome do livro: ");
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+
+            }
+
+        }while (opcaoEscolhida != 0) ;
+    }
 
 }
